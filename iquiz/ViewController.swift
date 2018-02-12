a91f4a366ebe166ebf658696931678fd81a16331
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-02-11 23:13:49 -0800

    this is the first push with functional code

---
+++ iquiz/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  iquiz
+//
+//  Created by Ben Nogawa on 2/11/18.
+//  Copyright © 2018 Ben Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        
+        cell.titleImage.image = UIImage(named: (list[indexPath.row] + ".jpg"))
+        cell.titleLabel.text = list[indexPath.row]
+        
+        cell.descriptionLabel.text = "This is the description. If there were description text it would go here. Imagine a yuuuge block of description text."
+        return(cell)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iquiz/ViewControllerTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerTableViewCell.swift
+//  iquiz
+//
+//  Created by Ben Nogawa on 2/11/18.
+//  Copyright © 2018 Ben Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
